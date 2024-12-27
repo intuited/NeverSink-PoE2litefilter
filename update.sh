56221df1bd470efdb8972397c6e28a447a261544
Ted Tibbetts <ted@circulated.ca>
2024-12-27 18:48:23 -0400

    custom version of the filter.

--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-cp -u NeverSinks\ Litefilter.filter ~/home/Documents/My\ Games/Path\ of\ Exile\ 2/NeverSinks\ Litefilter.filter
+cp -uv NeverSinks\ Litefilter.filter ~/home/Documents/My\ Games/Path\ of\ Exile\ 2/custom.filter
